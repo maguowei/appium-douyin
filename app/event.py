b43fbfcf8c2b04819f77379af1b6e98e3b64a8c2
maguowei <imaguowei@gmail.com>
2019-09-27 17:40:27 +0800

    update mitmproxt event

--- app/event.py
+++ app/event.py
@@ -1,50 +1,48 @@
 import json
-from pprint import pprint
 import mitmproxy.http
 import mitmproxy.proxy.protocol
 from app.service.mongo_service import db
 from app.service.redis_service import redis_client
-from app.douyin_driver import DouyinDriver, desired_caps
 
 
 class Events:
-    def __init__(self):
-        # self.driver = DouyinDriver(desired_caps)
-        # self.driver.click_by_name('同城')
-        pass
-
     def response(self, flow: mitmproxy.http.HTTPFlow):
         response = flow.response
         content = response.text
         url = flow.request.url
         # print(url)
-        # 附近列表
+
+        # 推荐
+        if '/aweme/v1/feed/' in url:
+            data = json.loads(content)
+            if data['status_code'] == 0 and data['has_more'] == 1:
+                for aweme in data['aweme_list']:
+                    aweme_id = aweme['aweme_id']
+                    db['feed_videos'].replace_one({'aweme_id': aweme_id}, aweme, upsert=True)
+                    redis_client.sadd('users', aweme['author_user_id'])
+        # 同城
         if '/aweme/v1/nearby/feed/' in url:
             data = json.loads(content)
-            # pprint(data)
             if data['status_code'] == 0 and data['has_more'] == 1:
-                # db['nearby'].insert_one(data)
-                # db['nearby_videos'].insert_many(data['aweme_list'])
                 for aweme in data['aweme_list']:
                     aweme_id = aweme['aweme_id']
                     db['nearby_videos'].replace_one({'aweme_id': aweme_id}, aweme, upsert=True)
                     redis_client.sadd('users', aweme['author_user_id'])
-
         # 关注
         elif '/v2/follow/feed/' in url:
             data = json.loads(content)
-            # pprint(data)
-            db['follow'].insert_one(data)
+            if data['status_code'] == 0 and data['has_more'] == 1:
+                for aweme in data['data']:
+                    aweme_id = aweme['aweme']['aweme_id']
+                    db['follow_videos'].replace_one({'aweme.aweme_id': aweme_id}, aweme, upsert=True)
+                    redis_client.sadd('users', aweme['author_user_id'])
 
         # 关注列表
         elif '/aweme/v1/user/follower/list/' in url:
             data = json.loads(content)
-            db['followers'].insert_one(data)
-            followers = data['followers']
-            for follower in followers:
-                user = {
-                    'uid': follower['uid'],
-                    'nickname': follower['nickname'],
-                }
-                redis_client.sadd('users', user['uid'])
-                # print(user)
+            if data['status_code'] == 0 and data['has_more'] == 1:
+                for follower in data['followers']:
+                    user_id = follower['uid']
+                    # nickname = follower['nickname'],
+                    db['follower_users'].replace_one({'uid': user_id}, follower, upsert=True)
+                    redis_client.sadd('users', user_id)
